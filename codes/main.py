90a84d4396a942bc5764c6274d4ffe3ab1c0a7b2
mame <86618354+mame7777@users.noreply.github.com>
2023-03-03 14:10:38 +0000

    [update] テーブル作成の関数の実装

--- codes/main.py
+++ codes/main.py
@@ -1,21 +1,39 @@
 import os
 
+import DB
 import dotenv
 import SlackAPI
 
+
+def init_main():
+    ChannelInfo = SlackAPI.GetChannelInfo()
+    # UserInfo = SlackAPI.GetUserInfo()
+    # ConversationInfo = SlackAPI.GetAllConversationInfo(list(ChannelInfo.keys()))
+    # print(SlackAPI.GetAllReplyInfo(list(ChannelInfo.keys()), ConversationInfo))
+    
+    cnx = DB.Connect()
+    DB_CT = DB.CreateTable(cnx=cnx)
+    # DB.Erase_All_Data(cnx)
+    DB_CT.channel()
+    DB_CT.user()
+    DB_CT.Conversation(channel_dict=ChannelInfo)  #これはチャンネル生成ごとに確認&実行
+    DB_CT.Reply(channel_dict=ChannelInfo)  #これはチャンネル生成ごとに確認&実行
+    DB.EndConnect(cnx)
 
 def main() -> int:
     # 環境変数の読み取り
     dotenv.load_dotenv()
     
-    SlackAPI.SendMessage("ログを取得するのスクリプトが開始されたよ")
+    init_main()
     
-    ChannelInfo = SlackAPI.GetChannelInfo()
-    UserInfo = SlackAPI.GetUserInfo()
-    ConversationInfo = SlackAPI.GetAllConversationInfo(list(ChannelInfo.keys()))
-    print(SlackAPI.GetAllReplyInfo(list(ChannelInfo.keys()), ConversationInfo))
+    # SlackAPI.SendMessage("ログを取得するのスクリプトが開始されたよ")
     
-    SlackAPI.SendMessage("ログを取得するのスクリプトが正常に終わったよ")
+    # ChannelInfo = SlackAPI.GetChannelInfo()
+    # UserInfo = SlackAPI.GetUserInfo()
+    # ConversationInfo = SlackAPI.GetAllConversationInfo(list(ChannelInfo.keys()))
+    # print(SlackAPI.GetAllReplyInfo(list(ChannelInfo.keys()), ConversationInfo))
+    
+    # SlackAPI.SendMessage("ログを取得するのスクリプトが正常に終わったよ")
     return 0
 
 
