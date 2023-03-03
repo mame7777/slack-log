8c7bd02fed44cd2cf3dfbd6184d0e0a967246835
mame <86618354+mame7777@users.noreply.github.com>
2023-03-03 16:31:04 +0000

    [add] DB登録(channel,user) テキストのDB登録できない

--- codes/DB.py
+++ codes/DB.py
@@ -57,10 +57,10 @@ class CreateTable:
         try:
             user_query = '''CREATE TABLE user (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                user_id VARCHAR(50) NOT NULL,
+                user_id VARCHAR(30) NOT NULL,
                 user_name VARCHAR(50) NOT NULL,
                 user_real_name VARCHAR(50) NOT NULL,
-                user_profile_img VARCHAR(500) NOT NULL
+                user_profile_img VARCHAR(300) NOT NULL
             );
             '''
             cursor.execute(user_query)
@@ -73,11 +73,11 @@ class CreateTable:
         print("Conversationのテーブル作成開始")
         for channel_id in channel_dict.keys():
             try:
-                conversation_query = '''CREATE TABLE '''+channel_id+'''_conversation (
+                conversation_query = '''CREATE TABLE IF NOT EXISTS '''+channel_id+'''_conversation (
                     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    ts INT NOT NULL,
+                    ts DOUBLE NOT NULL,
                     user_id VARCHAR(50) NOT NULL,
-                    text VARCHAR(1000) NOT NULL,
+                    text TEXT(2000) NOT NULL,
                     reply_count INT NOT NULL
                 );
                 '''
@@ -91,12 +91,12 @@ class CreateTable:
         print("Replyのテーブル作成開始")
         for channel_id in channel_dict.keys():
             try:
-                reply_query = '''CREATE TABLE '''+channel_id+'''_reply (
+                reply_query = '''CREATE TABLE IF NOT EXISTS '''+channel_id+'''_reply (
                     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                    conversation_ts INT NOT NULL,
-                    reply_ts INT NOT NULL,
+                    conversation_ts DOUBLE NOT NULL,
+                    reply_ts DOUBLE NOT NULL,
                     user_id VARCHAR(50) NOT NULL,
-                    text VARCHAR(1000) NOT NULL
+                    text TEXT(2000) NOT NULL
                 );
                 '''
                 cursor.execute(reply_query)
@@ -109,7 +109,56 @@ class Insert:
     
     def __init__(self, cnx: mysql.connector):
         self.cnx = cnx
-
+    
+    def channel(self, channel_dict: dict) -> None:
+        cursor = self.cnx.cursor()
+        insert_channel = ("INSERT INTO channel "
+                          "(channel_id, channel_name, topic, purpose) "
+                          "VALUES (%s, %s, %s, %s)")
+        for channel_id in channel_dict.keys():
+            channel_name = channel_dict[channel_id]["name"]
+            channel_topic = channel_dict[channel_id]["topic"]
+            channel_purpose = channel_dict[channel_id]["purpose"]
+            data_channel = (channel_id, channel_name, channel_topic, channel_purpose)
+            cursor.execute(insert_channel, data_channel)
+        self.cnx.commit()
+    
+    def user(self, user_dict: dict) -> None:
+        cursor = self.cnx.cursor()
+        insert_user = ("INSERT INTO user "
+                          "(user_id, user_name, user_real_name, user_profile_img) "
+                          "VALUES (%s, %s, %s, %s)")
+        for user_id in user_dict.keys():
+            user_name = user_dict[user_id]["name"]
+            user_real_name = user_dict[user_id]["real_name"]
+            user_profile_img = user_dict[user_id]["profile_img"]
+            data_user = (user_id, user_name, user_real_name, user_profile_img)
+            cursor.execute(insert_user, data_user)
+        self.cnx.commit()
+    
+    def conversation(self, conversation_dict: dict) -> None:
+        cursor = self.cnx.cursor()
+        for channel_conversation in conversation_dict.keys():
+            table_conversation = channel_conversation+"_conversation"
+            insert_conversation =(table_conversation+
+                                " (ts, user_id, text, reply_count)"
+                                " VALUES (%f, %s, %s, %d)")
+            for ts in conversation_dict[channel_conversation].keys():
+                ts_float = float(ts)
+                user_id = conversation_dict[channel_conversation][ts]["user"]
+                text = conversation_dict[channel_conversation][ts]["text"]
+                # text_list = conversation_dict[channel_conversation][ts]["text"].splitlines()
+                # text = ("~".join(text_list)).replace(" ", "")
+                reply_count = int(conversation_dict[channel_conversation][ts]["reply_count"])
+                # query_conversation = """INSERT INTO %s_conversation 
+                #                    (ts, user_id, text, reply_count) 
+                #                    VALUES (%f, %s, %s, %d)""" % (table_conversation, ts_float, user_id, text, reply_count)
+                # print(query_conversation)
+                data_conversation = (table_conversation, ts_float, user_id, text, reply_count)
+                # cursor.execute(query_conversation)
+                cursor.execute(insert_conversation,data_conversation)
+        self.cnx.commit()
+        
 def CreateTableTest(cnx: mysql.connector) -> None:
     cursor = cnx.cursor()
     # try:
