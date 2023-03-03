561f6f2ba14ea2e77b8d96955292ef2ab9eaa7ff
mame <86618354+mame7777@users.noreply.github.com>
2023-03-03 07:00:09 +0000

    [update] 返信を取得できるように

--- codes/app.py
+++ codes/app.py
@@ -2,6 +2,7 @@ import requests
 import json
 import dotenv
 import os
+import time
 
 # 辞書型のチャンネルデータを取得
 
@@ -40,65 +41,126 @@ def GetUserInfo() -> dict:
     # 辞書型のユーザーデータを生成
     user_dict = dict()
     for i in user_data["members"]:
-        # 名称未設定の人の分をスキップ
-        try:
-            real_name = i["real_name"]
-        except KeyError:
-            real_name = ""
-
         # 画像を取得できない人の分をスキップ
         # 他の画像を探すようにしたいね
-        try:
-            profile_img = i["profile"]["image_72"]
-        except KeyError:
-            profile_img = ""
+        profile_img = i.get("profile", "")
 
         user_dict[i["id"]] = {"name": i["name"],
-                              "real_name": real_name,
-                              "profile_img": profile_img}
+                              "real_name": i.get("real_name", ""),
+                              "profile_img": profile_img.get("image_72", "")}
 
     return user_dict
 
 
 def GetConversationInfo(channel_id: str) -> dict:
-    # channel_id = "C049P5T2F7H"
     url = "https://slack.com/api/conversations.history"
-    headers = {"Authorization": "Bearer "+str(os.environ.get("SLACK_BOT_TOKEN"))}
+    headers = {"Authorization": "Bearer " +
+               str(os.environ.get("SLACK_BOT_TOKEN"))}
     payload = {"channel": channel_id}
-    
-    # , "cursor":""
+
     # メッセージ一覧の取得
     response = requests.get(url, headers=headers, params=payload)
     conversation_data = response.json()
-    # print(json.dumps(conversation_data, indent=2, ensure_ascii=False))
-    
+    print(json.dumps(conversation_data, indent=2, ensure_ascii=False))
+
     conversation_dict = dict()
-    for i in conversation_data["messages"]:
-        # try:
-        #     files=i["files"]
-        # except KeyError:
-        #     files=""
-        
-        # if files != "":
-        #     files_name = i["files"]["name"]
-        #     files_url = i["files"]["url_private"]
-        # else:
-        #     files_name =""
-        #     files_url = ""
-        conversation_dict[i["ts"]] = {"user": i["user"], \
-                                    "text": i["text"], }\
-                                    # "files_name": files_name, \
-                                    # "files_url": files_url}
+
+    if conversation_data["ok"]:
+        while True:
+            for i in conversation_data["messages"]:
+                conversation_dict[i["ts"]] = {
+                    "user": i["user"], "text": i["text"], "reply_count": i.get("reply_count", "0")}
+
+            if conversation_data["has_more"]:
+                # APIレートの調整
+                time.sleep(1.35)
+                payload = {"channel": channel_id,
+                           "cursor": conversation_data["response_metadata"]["next_cursor"]}
+                response = requests.get(url, headers=headers, params=payload)
+                conversation_data = response.json()
+            else:
+                break
 
     return conversation_dict
 
 
-def main():
+def GetAllConversationInfo(channnel_list: list) -> dict:
+    ALLconversation_dict = dict()
+    for channel_id in channnel_list:
+        ALLconversation_dict[channel_id] = GetConversationInfo(channel_id)
+        # APIレートの調整
+        time.sleep(1.35)
+
+        # if channel_id == "C1GQGJURK":
+        #     break
+
+    return ALLconversation_dict
+
+
+def GetReplysInfo(channel_id: str, history_ts_list: list) -> dict:
+    reply_dict = dict()
+    for history_ts in history_ts_list:
+        url = "https://slack.com/api/conversations.replies"
+        headers = {"Authorization": "Bearer " +
+                   str(os.environ.get("SLACK_BOT_TOKEN"))}
+        payload = {"channel": channel_id, "ts": history_ts}
+
+        # メッセージ一覧の取得
+        response = requests.get(url, headers=headers, params=payload)
+        reply_data = response.json()
+        print(json.dumps(reply_data, indent=2, ensure_ascii=False))
+
+        if reply_data["ok"]:
+            reply_dict_temp = dict()
+            while True:
+                for i in reply_data["messages"]:
+                    reply_dict_temp[i["ts"]] = {
+                        "user": i["user"], "text": i["text"]}
+
+                if reply_data["has_more"]:
+                    # APIレートの調整
+                    time.sleep(1.35)
+                    payload = {"channel": channel_id, "ts": history_ts,
+                               "cursor": reply_data["response_metadata"]["next_cursor"]}
+                    response = requests.get(
+                        url, headers=headers, params=payload)
+                    reply_data = response.json()
+                else:
+                    break
+            reply_dict[history_ts] = {"reply_dict": reply_dict_temp}
+
+        # APIレートの調整
+        time.sleep(1.35)
+
+    return reply_dict
+
+
+def GetAllReplyInfo(channnel_list: list, ALLconversation_dict: dict) -> dict:
+    ALLReply_dict = dict()
+    for channel_id in channnel_list:
+        conversation_dict = ALLconversation_dict[channel_id]
+
+        ts_HasReply_list = list()
+        for ts_in_loop in conversation_dict.keys():
+            if int(conversation_dict[ts_in_loop]["reply_count"]) > 0:
+                ts_HasReply_list.append(ts_in_loop)
+
+        ALLReply_dict[channel_id] = GetReplysInfo(channel_id, ts_HasReply_list)
+
+        # if channel_id == "C1GQGJURK":
+        #     break
+
+    return ALLReply_dict
+
+
+def main() -> int:
     # 環境変数の読み取り
     dotenv.load_dotenv()
-    # GetChannelInfo()
-    # GetUserInfo()
-    GetConversationInfo("C049P5T2F7H")
+    ChannelInfo = GetChannelInfo()
+    UserInfo = GetUserInfo()
+    ConversationInfo = GetAllConversationInfo(list(ChannelInfo.keys()))
+    print(GetAllReplyInfo(list(ChannelInfo.keys()), ConversationInfo))
+    return 0
 
 
 if __name__ == '__main__':
